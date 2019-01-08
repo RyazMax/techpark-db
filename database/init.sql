2d294d1fe93a36200a81f648796c2706c23a53f2
Ryazanov Maxim <ryazanovms@yandex.ru>
2019-01-08 18:51:46 +0300

    Upd index

--- database/init.sql
+++ database/init.sql
@@ -152,11 +152,11 @@ DROP INDEX IF EXISTS posts_thread_id_idx;
 DROP INDEX IF EXISTS post_mpath_idx;
 DROP INDEX IF EXISTS post_mpath_desc_idx;
 
-CREATE INDEX IF NOT EXISTS forum_user_nickname_email_idx ON forum_user(nickname, email);
+CREATE INDEX IF NOT EXISTS forum_user_nickname_email_idx ON forum_user(LOWER(nickname), LOWER(email));
 
-CREATE INDEX IF NOT EXISTS thread_slug_idx on thread (slug);
-CREATE INDEX IF NOT EXISTS thread_forum_created_idx ON thread (forum, created);
-CREATE INDEX IF NOT EXISTS vote_username_thread_idx ON vote (nickname, thread);
+CREATE INDEX IF NOT EXISTS thread_slug_idx on thread (LOWER(slug));
+CREATE INDEX IF NOT EXISTS thread_forum_created_idx ON thread (LOWER(forum), created);
+CREATE INDEX IF NOT EXISTS vote_username_thread_idx ON vote (LOWER(nickname), thread);
 
 CREATE INDEX IF NOT EXISTS posts_thread_id_idx ON post(thread, created);
 CREATE INDEX IF NOT EXISTS post_mpath_idx ON post((mpath[1]), (mpath[2:]));
